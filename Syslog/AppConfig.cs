793158de85b7318c6b9d871019458cd68a2edc8b
Marko Bogdanovic <mamaveb@gmail.com>
2016-11-13 16:32:45 +0100

    Add multiple config files

--- Syslog/AppConfig.cs
+++ Syslog/AppConfig.cs
@@ -9,10 +9,22 @@ namespace Syslog
 {
     public static class AppConfig
     {
-        public static readonly bool IS_MAIN_SYSLOG = Convert.ToBoolean(ConfigurationSettings.AppSettings["isMainSyslog"]);
-        public static readonly int MY_PORT = Convert.ToInt32(ConfigurationSettings.AppSettings["myPort"]);
-        public static readonly string BACKUP_ADDRESS = ConfigurationSettings.AppSettings["backupAddress"];
-        public static readonly int BACKUP_PORT = Convert.ToInt32(ConfigurationSettings.AppSettings["backupPort"]);
-        public static readonly bool HAS_BACKUP = Convert.ToBoolean(ConfigurationSettings.AppSettings["hasBackup"]);
+        static AppConfig()
+        {
+            //ExeConfigurationFileMap configFileMap = new ExeConfigurationFileMap();
+            //configFileMap.ExeConfigFilename = "App.config";
+
+            //// Get the mapped configuration file
+            //Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configFileMap, ConfigurationUserLevel.None);
+
+            ////now on use config object
+
+            //AppSettingsSection section = (AppSettingsSection)config.GetSection("appSettings");
+        }
+        public static readonly bool IS_MAIN_SYSLOG = Convert.ToBoolean(ConfigurationManager.AppSettings["isMainSyslog"]);
+        public static readonly int MY_PORT = Convert.ToInt32(ConfigurationManager.AppSettings["myPort"]);
+        public static readonly string BACKUP_ADDRESS = ConfigurationManager.AppSettings["backupAddress"];
+        public static readonly int BACKUP_PORT = Convert.ToInt32(ConfigurationManager.AppSettings["backupPort"]);
+        public static readonly bool HAS_BACKUP = Convert.ToBoolean(ConfigurationManager.AppSettings["hasBackup"]);
     }
 }
